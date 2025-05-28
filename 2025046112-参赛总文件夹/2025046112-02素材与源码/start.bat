af84d3078ae0a3b933f07d96437018aada63c57a
kpmark <2585050765@qq.com>
2025-05-28 19:59:02 +0800

    删除部分参赛信息

--- 2025046112-参赛总文件夹/2025046112-02素材与源码/start.bat
+++
@@ -1,4 +0,0 @@
-start powershell -NoExit -Command "cd Raspb; python http_client.py"
-start powershell -NoExit -Command "cd Raspb; python data_mock.py"
-
-start powershell -NoExit -Command "cd Server; python server.py"
